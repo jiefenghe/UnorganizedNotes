51255795b8addff8e3eb1abd59d4cd9bd865a315
yeti2018 <jiefenghaspower@gmail.com>
2018-03-11 11:18:22 -0700

    express session
    set up the Express server to use express-session to track authenticated users to provide access to secure resources

--- NodeJS/auth.js
+++ NodeJS/auth.js
@@ -66,3 +66,63 @@ function auth (req, res, next) {
 }
 
 app.use(auth);
+
+/* Express Session
+Standard practice up until about 2007 was to store session data on the server, typically in a file, a database,
+or a distributed memory cache. Cookies were used only to store the session keys—short strings that uniquely
+identified a user and allowed the server to retrieve session data from the session store as necessary. This had
+little effect on client/server latency since the cookies were small, and it was reasonably secure since users
+couldn’t tamper with the actual contents of their session data; the best a malicious user could hope to do was
+to sniff out another user’s session id and impersonate them, which could be easily prevented using SSL.
+Reference: https://wonko.com/post/why-you-probably-shouldnt-use-cookies-to-store-session-data
+*/
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+function auth (req, res, next) {
+    console.log(req.session);
+
+    if (!req.session.user) {
+        var authHeader = req.headers.authorization;
+        if (!authHeader) {
+            var err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');                        
+            err.status = 401;
+            next(err);
+            return;
+        }
+        var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+        var user = auth[0];
+        var pass = auth[1];
+        if (user == 'admin' && pass == 'password') {
+            req.session.user = 'admin';
+            next(); // authorized
+        } else {
+            var err = new Error('You are not authenticated!');
+            res.setHeader('WWW-Authenticate', 'Basic');
+            err.status = 401;
+            next(err);
+        }
+    }
+    else {
+        if (req.session.user === 'admin') {
+            console.log('req.session: ',req.session);
+            next();
+        }
+        else {
+            var err = new Error('You are not authenticated!');
+            err.status = 401;
+            next(err);
+        }
+    }
+}
+
+app.use(auth);
