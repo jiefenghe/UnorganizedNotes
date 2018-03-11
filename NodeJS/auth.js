bf5bef53aa72901c60c647ddf331a646dd1576e8
yeti2018 <jiefenghaspower@gmail.com>
2018-03-11 16:13:43 -0700

    user authentication with passport

--- NodeJS/auth.js
+++ NodeJS/auth.js
@@ -259,3 +259,75 @@ function auth (req, res, next) {
     }
   }
 }
+
+// using passport
+// update User Schema and Model
+// update user.js
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var User = new Schema({
+    admin:   {
+        type: Boolean,
+        default: false
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
+// add passport-based authentication
+// add authenticate.js
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('./models/user');
+
+exports.local = passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// update users.js
+var passport = require('passport');
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json');
+      res.json({err: err});
+    }
+    else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, status: 'Registration Successful!'});
+      });
+    }
+  });
+});
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json({success: true, status: 'You are successfully logged in!'});
+});
+
+// update app.js
+var passport = require('passport');
+var authenticate = require('./authenticate');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function auth (req, res, next) {
+    console.log(req.user);
+
+    if (!req.user) {
+      var err = new Error('You are not authenticated!');
+      res.setHeader('WWW-Authenticate', 'Basic');                          
+      err.status = 401;
+      next(err);
+    }
+    else {
+          next();
+    }
+}
