9bf16b7aac9dee0549265a92721b08096e10e9e7
yeti2018 <jiefenghaspower@gmail.com>
2018-03-11 10:58:12 -0700

    cookie

--- NodeJS/auth.js
+++ NodeJS/auth.js
@@ -24,3 +24,45 @@ function auth (req, res, next) {
 }
 
 app.use(auth);
+
+// Cookie
+// first time require basic authentication, later only check the cookie
+app.use(cookieParser('some-random-key-for-security'));
+
+function auth (req, res, next) {
+
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+    }
+  }
+  else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
+  }
+}
+
+app.use(auth);
